d6470a8572c4f4986cd9f6b7171c8d7eee239384
jerry-chan <jerry.chan@uha.fr>
2023-11-20 09:14:31 +0100

    Modélisation rotoide 0_1_2_3
    mise en place des 4 rotoides

---
+++ Assets/MoveRotoide_1.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRotoide_1 : MonoBehaviour
+{
+        public float moveSpeed = 10.0f;
+     public float stored_moveSpeed = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+
+        transform.Rotate(Vector3.up,45);
+        
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Y)) {
+transform.Rotate(Vector3.up,45);
+}
+if ( Input.GetKeyDown(KeyCode.Z)) {
+transform.Rotate(Vector3.back,45);
+}
+
+if ( Input.GetKeyDown(KeyCode.X)) {
+transform.Rotate(Vector3.left,45);
+}
+    }
+}
