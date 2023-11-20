ea197c641b28a730fd4ab1746e1d918f6d953950
jerry-chan <jerry.chan@uha.fr>
2023-11-20 09:57:02 +0100

    commit
    fin robot

--- Assets/MoveRotoide.cs
+++ Assets/MoveRotoide.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRotoide_3 : MonoBehaviour
+public class MoveRotoide : MonoBehaviour
 {
+    public KeyCode key = KeyCode.U ;
+      
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-         if ( Input.GetKeyDown(KeyCode.I)) {
+        if ( Input.GetKeyDown(key)) {
             transform.Rotate(Vector3.up,45);
         }
     }
--- Assets/Move_Prismatique.cs
+++ Assets/Move_Prismatique.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRotoide_0 : MonoBehaviour
+public class Move_Prismatique : MonoBehaviour
 {
-      
     // Start is called before the first frame update
     void Start()
-    { 
-
-
-        transform.Rotate(Vector3.up,45);
+    {
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKeyDown(KeyCode.T)) {
-            transform.Rotate(Vector3.up,45);
-        }
+       
     }
 }
