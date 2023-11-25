0b5178ce212d13a9fe0d3cce79884a1a7a35c21b
jerry-chan <jerry.chan@uha.fr>
2023-11-25 19:20:15 +0100

    ma branche a des bugs
    aaaa

--- Assets/MoveRotoide_0.cs
+++ Assets/MoveRotoide_0.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move_Prismatique : MonoBehaviour
+public class MoveRotoide_0 : MonoBehaviour
 {
+      
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+
+
+        transform.Rotate(Vector3.up,45);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if ( Input.GetKeyDown(KeyCode.T)) {
+            transform.Rotate(Vector3.up,45);
+        }
     }
 }
--- Assets/MoveRotoide_3.cs
+++ Assets/MoveRotoide_3.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRotoide : MonoBehaviour
+public class MoveRotoide_3 : MonoBehaviour
 {
-    public KeyCode key = KeyCode.U ;
-      
     // Start is called before the first frame update
     void Start()
-    { 
-
-
+    {
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKeyDown(key)) {
+         if ( Input.GetKeyDown(KeyCode.I)) {
             transform.Rotate(Vector3.up,45);
         }
     }
